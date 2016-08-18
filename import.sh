6affdb4e0fc5147c1ba460bb383e579004998c5d
Marcel <MTRNord@users.noreply.github.com>
2016-08-18 21:52:04 +0200

    Update import.sh

--- import.sh
+++ import.sh
@@ -12,4 +12,4 @@ echo "INSERT INTO mapit_type (code, description) VALUES ('$CODE', '$DESCRIPTION'
 cd /data; unzip $NAME.zip; rm $NAME.zip
 # TODO do proper projection conversion using -s_srs "EPSG:28350" -t_srs "EPSG:4326"
 ogr2ogr -f "KML" -dsco NameField=$NAME_FIELD /data/$FILE_NAME.kml /data/$FILE_NAME.shp
-su -l -c "/var/www/mapit/mapit/manage.py mapit_import --country_code DE --area_type_code $CODE --name_type_code $CODE --generation_id 1 --commit /data/$FILE_NAME.kml" mapit
+su -l -c "/var/www/mapit/mapit/manage.py mapit_import --country_code DE --area_type_code $FILE_NAME --name_type_code $FILE_NAME --generation_id 1 --commit /data/$FILE_NAME.kml" mapit
