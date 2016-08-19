5e0bf6139ac717406c60ff1cd6e6d520fcedc891
Marcel <MTRNord@users.noreply.github.com>
2016-08-19 21:12:37 +0200

    Update import.sh

--- import.sh
+++ import.sh
@@ -6,9 +6,11 @@ DESCRIPTION=$3
 NAME_FIELD=$4
 FILE_NAME=$5
 NAME_CODE=$6
+NAME_CODE_DESC=$7
 
 service postgresql restart && sleep 20
 echo "INSERT INTO mapit_type (code, description) VALUES ('$CODE', '$DESCRIPTION'); INSERT INTO mapit_nametype (code, description) VALUES ('$CODE', '$DESCRIPTION');" | su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; psql mapit" mapit
+echo "INSERT INTO mapit_type (code, description) VALUES ('$NAME_CODE', '$NAME_CODE_DESC'); INSERT INTO mapit_nametype (code, description) VALUES ('$NAME_CODE', '$NAME_CODE_DESC');" | su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; psql mapit" mapit
 
 echo "unzip"
 cd /data; unzip $NAME.zip; rm $NAME.zip
