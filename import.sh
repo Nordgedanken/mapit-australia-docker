4089f7ecbc31ff7af12c0c25972355f2428c7cc1
Marcel <MTRNord@users.noreply.github.com>
2016-08-19 20:57:45 +0200

    Update import.sh

--- import.sh
+++ import.sh
@@ -5,6 +5,7 @@ CODE=$2
 DESCRIPTION=$3
 NAME_FIELD=$4
 FILE_NAME=$5
+NAME_CODE=$6
 
 service postgresql restart && sleep 20
 echo "INSERT INTO mapit_type (code, description) VALUES ('$CODE', '$DESCRIPTION'); INSERT INTO mapit_nametype (code, description) VALUES ('$CODE', '$DESCRIPTION');" | su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; psql mapit" mapit
@@ -14,8 +15,5 @@ cd /data; unzip $NAME.zip; rm $NAME.zip
 # TODO do proper projection conversion using -s_srs "EPSG:28350" -t_srs "EPSG:4326"
 echo "convert"
 ogr2ogr -f "KML" -dsco NameField=$NAME_FIELD /data/$FILE_NAME.kml /data/$FILE_NAME.shp
-ls -la /data
-ls -la /data/$FILE_NAME.kml
 echo "insert"
-su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --country_code DEU --area_type_code LGA --name_type_code LGA --generation_id 1 --commit /data/$FILE_NAME.kml" mapit
-#su -l -c "/var/www/mapit/mapit/manage.py mapit_import --country_code DEU --area_type_code $CODE --name_type_code $CODE --name_field "NAME_ENGLISH" --generation_id 1 --commit /data/$FILE_NAME.kml" mapit
+su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --country_code DEU --area_type_code $CODE --name_type_code $NAME_CODE --generation_id 1 --commit /data/$FILE_NAME.kml" mapit
