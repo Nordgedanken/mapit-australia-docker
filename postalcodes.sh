04c9db952ba2010735d1225df8069c622f918235
Marcel <MTRNord@users.noreply.github.com>
2016-08-21 19:44:49 +0200

    Update postalcodes.sh

--- postalcodes.sh
+++ postalcodes.sh
@@ -7,4 +7,4 @@ LAT=$4
 
 service postgresql restart && sleep 20
 
-su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import_postal_codes --code-field $CODES --coord-field-lon $LONG --coord-field-lat $LAT /tmp/$FILE_NAME.csv --commit" mapit
+su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import_postal_codes --code-field $CODES --coord-field-lon $LONG --coord-field-lat $LAT /tmp/$FILE_NAME.csv" mapit
