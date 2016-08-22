5924ee281f4cefc0c085c804aefd3a9f10da1ffd
Marcel <MTRNord@users.noreply.github.com>
2016-08-22 18:15:10 +0200

    Update countries.py

--- mapit_de/countries.py
+++ mapit_de/countries.py
@@ -1,19 +1,12 @@
 import re
 
 # SRID to also output area geometry information in
-#area_geometry_srid = 32633
+area_geometry_srid = 4326
 
 
 # Norwegian postcodes are four digits. Some put "no-" in front, but
 # this is ignored here.
 def is_valid_postcode(pc):
-    if re.match('^0[1-9]\d\d(?<!0100)0|0[1-9]\d\d[1-9]|[1-9]\d{3}[0-8]|[1-9]\d{3}(?<!9999)9$', pc):
-        return True
-    return False
-
-
-# Should match one, two and three digits.
-def is_valid_partial_postcode(pc):
-    if re.match('^0[1-9]\d\d(?<!0100)0|0[1-9]\d\d[1-9]|[1-9]\d{3}[0-8]|[1-9]\d{3}(?<!9999)9$', pc):
+    if re.match('^(?!01000|99999)(0[1-9]\d{3}|[1-9]\d{4})$', pc):
         return True
     return False
