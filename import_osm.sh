c05d2fd6c8226f835c13bca14f0a429275e6a091
Marcel <MTRNord@users.noreply.github.com>
2016-08-22 15:06:25 +0200

    Update import_osm.sh

--- import_osm.sh
+++ import_osm.sh
@@ -7,7 +7,7 @@ CODE_FIELD=$4
 CODE_DESC=$5
 
 service postgresql restart && sleep 20
-#echo "INSERT INTO mapit_type (code, description) VALUES ('binfo', 'BoundaryInfo'); INSERT INTO mapit_nametype (code, description) VALUES ('binfo', 'BoundaryInfo');" | su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; psql mapit" mapit
+echo "INSERT INTO mapit_type (code, description) VALUES ('binfo', 'BoundaryInfo'); INSERT INTO mapit_nametype (code, description) VALUES ('binfo', 'BoundaryInfo');" | su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; psql mapit" mapit
 #echo "OSM TYPE"
 #echo "INSERT INTO mapit_type (code, description) VALUES ('osm_rel', 'OSM relation ID'); INSERT INTO mapit_nametype (code, description) VALUES ('osm_rel', 'OSM relation ID');" | su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; psql mapit" mapit
 
