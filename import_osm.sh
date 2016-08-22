4c6e0f84ff32615609682cd07d25a7f72257c42a
Marcel <MTRNord@users.noreply.github.com>
2016-08-22 21:40:26 +0200

    Update import_osm.sh

--- import_osm.sh
+++ import_osm.sh
@@ -17,6 +17,6 @@ then
    echo "File GeoJson exists."
    su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --generation_id 1 --area_type_code $CODE --name_type_code binfo --country_code DE --name_field $NAME_FIELD --code_field $CODE_FIELD --code_type osm_rel data/$NAME.GeoJson --commit" mapit
 else
-   echo "File Shapefile does not exist.",
+   echo "File Shapefile exists.",
    su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --generation_id 1 --area_type_code $CODE --name_type_code binfo --country_code DE --name_field $NAME_FIELD --code_field $CODE_FIELD --code_type osm_rel data/$NAME.shp --commit" mapit
 fi
