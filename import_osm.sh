1e016afa6624e85c9cbf8f94fd4f7cf84acf2901
Marcel <MTRNord@users.noreply.github.com>
2016-08-22 12:35:21 +0200

    Update import_osm.sh

--- import_osm.sh
+++ import_osm.sh
@@ -8,8 +8,8 @@ CODE_DESC=$5
 
 service postgresql restart && sleep 20
 echo "INSERT INTO mapit_type (code, description) VALUES ('binfo', 'BoundaryInfo'); INSERT INTO mapit_nametype (code, description) VALUES ('binfo', 'BoundaryInfo');" | su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; psql mapit" mapit
-#echo "OSM TYPE"
-#echo "INSERT INTO mapit_type (code, description) VALUES ('OSM', 'OpenStreetMap'); INSERT INTO mapit_nametype (code, description) VALUES ('OSM', 'OpenStreetMap');" | su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; psql mapit" mapit
+echo "OSM TYPE"
+echo "INSERT INTO mapit_type (code, description) VALUES ('osm_rel', 'OSM relation ID'); INSERT INTO mapit_nametype (code, description) VALUES ('osm_rel', 'OSM relation ID');" | su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; psql mapit" mapit
 
 echo "Read Data"
 su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --generation_id 1 --area_type_code $CODE --name_type_code binfo --country_code DE --name_field $NAME_FIELD --code_field $CODE_FIELD --code_type osm_rel /data/$NAME.shp --commit" mapit
