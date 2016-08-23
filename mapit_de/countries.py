41343ed93cd7f56e36df5ebb879ad85085386d03
Marcel <MTRNord@users.noreply.github.com>
2016-08-23 20:18:01 +0200

    Update countries.py

--- mapit_de/countries.py
+++ mapit_de/countries.py
@@ -1,16 +1,5 @@
 import re
 
-# SRID to also output area geometry information in
-area_geometry_srid = 4326
-
-def is_valid_postcode(pc):
-    #if re.match('^(?!01000|99999)(0[1-9]\d{3}|[1-9]\d{4})$', pc):
-    #    return True
-    return True
-
-
-import re
-
 from django.core.urlresolvers import reverse, NoReverseMatch
 from django.http import HttpResponseRedirect
 
