3f2908e499dd21d4fc80836ddd34869777a32350
Marcel <MTRNord@users.noreply.github.com>
2016-08-23 20:10:45 +0200

    Update import_osm.sh

--- import_osm.sh
+++ import_osm.sh
@@ -12,11 +12,13 @@ echo "INSERT INTO mapit_type (code, description) VALUES ('binfo', 'BoundaryInfo'
 #echo "INSERT INTO mapit_type (code, description) VALUES ('osm_rel', 'OSM relation ID'); INSERT INTO mapit_nametype (code, description) VALUES ('osm_rel', 'OSM relation ID');" | su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; psql mapit" mapit
 
 echo "Read Data"
-if [ -f data/$NAME.GeoJson ];
+if [ -f /data/$NAME.GeoJson ];
 then
    echo "File GeoJson exists."
-   su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --generation_id 1 --area_type_code O02 --name_type_code binfo --country_code DE --name_field $NAME_FIELD --code_field $CODE_FIELD --code_type osm_rel data/$NAME.GeoJson --preserve --commit" mapit
+   cd /data
+   su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --generation_id 1 --area_type_code O02 --name_type_code binfo --country_code DE --name_field $NAME_FIELD --code_field $CODE_FIELD --code_type osm_rel /data/$NAME.GeoJson --preserve --commit" mapit
 else
-   echo "File Shapefile exists.",
-   su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --generation_id 1 --area_type_code O02 --name_type_code binfo --country_code DE --name_field $NAME_FIELD --code_field $CODE_FIELD --code_type osm_rel data/$NAME.shp --preserve --commit" mapit
+   echo "File Shapefile exists."
+   cd /data
+   su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --generation_id 1 --area_type_code O02 --name_type_code binfo --country_code DE --name_field $NAME_FIELD --code_field $CODE_FIELD --code_type osm_rel /data/$NAME.shp --preserve --commit" mapit
 fi
