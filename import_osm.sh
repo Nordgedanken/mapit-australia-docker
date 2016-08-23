4815aa1e7425f5d95c4b60571f095caaf827c53e
Marcel <MTRNord@users.noreply.github.com>
2016-08-23 17:13:36 +0200

    Update import_osm.sh

--- import_osm.sh
+++ import_osm.sh
@@ -15,8 +15,8 @@ echo "Read Data"
 if [ -f data/$NAME.GeoJson ];
 then
    echo "File GeoJson exists."
-   su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --generation_id 1 --area_type_code $CODE --name_type_code binfo --country_code DE --name_field $NAME_FIELD --code_field $CODE_FIELD --code_type osm_rel data/$NAME.GeoJson --commit" mapit
+   su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --generation_id 1 --name_type_code binfo --country_code DE --name_field $NAME_FIELD --code_field $CODE_FIELD --code_type osm_rel data/$NAME.GeoJson --commit" mapit
 else
    echo "File Shapefile exists.",
-   su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --generation_id 1 --area_type_code $CODE --name_type_code binfo --country_code DE --name_field $NAME_FIELD --code_field $CODE_FIELD --code_type osm_rel data/$NAME.shp --commit" mapit
+   su -l -c ". /var/www/mapit/virtualenv-mapit/bin/activate; /var/www/mapit/mapit/manage.py mapit_import --generation_id 1 --name_type_code binfo --country_code DE --name_field $NAME_FIELD --code_field $CODE_FIELD --code_type osm_rel data/$NAME.shp --commit" mapit
 fi
